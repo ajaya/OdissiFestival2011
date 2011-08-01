294396ee9e1f7d39678e51826fbfc0cf3b8a8b27
ajaya <baudhia@gmail.com>
2011-07-31 18:37:17 -0700

    First release

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+#ENV['RAILS_RELATIVE_URL_ROOT'] = "/festival2011"
+
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Odissi2011::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odissi2011::Application.config.secret_token = '982db0229e172375cc54de63217bcad824906c3f06d85e5ff104e44ee048d1b322554e38ce257fe2433119b5e34da959915299e4ee4e5b114459e0a75699439d'
